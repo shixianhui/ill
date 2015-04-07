a2be646694511d57bf860718840ce223fb15d6f9
shixianhui <610197056@qq.com>
2015-04-07 20:25:42 +0800

    ss

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,14 @@
 class WelcomeController < ApplicationController
   def index
   	@child_name = {
-  		"1" => "儿童感冒气象风险",
-  		"2" => "儿童哮喘气象风险"
+  		"1" => {
+  			link:"/child/ccold",
+  			word:"儿童感冒气象风险"
+  			},
+  		"2" =>{
+  			link:"/child/ccold",
+  			word:"儿童哮喘气象风险"
+  			} 
   	}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'child/ccold'
+  get 'child/ccold' 
 
   get 'welcome/index'
 
